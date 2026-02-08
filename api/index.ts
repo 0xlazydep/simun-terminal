9e59656321a7fa80b5965a82bf1c76a2ddd000b8
0xlazydep <km658397@gmail.com>
2026-02-09 02:11:11 +0700

    change data

---
+++ api/index.ts
@@ -0,0 +1,44 @@
+import express, { type Request, type Response, type NextFunction } from "express";
+import { createServer } from "http";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+const httpServer = createServer(app);
+
+declare module "http" {
+  interface IncomingMessage {
+    rawBody: unknown;
+  }
+}
+
+app.use(
+  express.json({
+    verify: (req, _res, buf) => {
+      req.rawBody = buf;
+    },
+  }),
+);
+
+app.use(express.urlencoded({ extended: false }));
+
+const ready = (async () => {
+  await registerRoutes(httpServer, app);
+
+  app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
+    const status = err.status || err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    console.error("Internal Server Error:", err);
+
+    if (res.headersSent) {
+      return next(err);
+    }
+
+    return res.status(status).json({ message });
+  });
+})();
+
+export default async function handler(req: Request, res: Response) {
+  await ready;
+  return app(req, res);
+}
